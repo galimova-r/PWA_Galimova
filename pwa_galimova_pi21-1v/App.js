c5740664da7ceec3dd2dec4edd8b8b482ccd72eb
galimova-r <113368059+galimova-r@users.noreply.github.com>
2025-04-19 10:06:17 +0300

    Add files via upload

---
+++ pwa_galimova_pi21-1v/App.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) { // Проверка поддержки Service Worker
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js'); // Регистрация sw.js при загрузке страницы
+  });
+}
