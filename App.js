2b05aff1ab72df8c0d1d970b831ef06750d3283d
galimova-r <113368059+galimova-r@users.noreply.github.com>
2025-04-19 10:45:37 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-if ('serviceWorker' in navigator) { // Проверка поддержки Service Worker
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js'); // Регистрация sw.js при загрузке страницы
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => console.log('SW registered: ', registration))
+      .catch(error => console.log('SW registration failed: ', error));
   });
 }
