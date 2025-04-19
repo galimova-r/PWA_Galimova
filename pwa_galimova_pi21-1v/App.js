35f62c8402b920f00c5cf18b4c8e605c7d30b76e
galimova-r <113368059+galimova-r@users.noreply.github.com>
2025-04-19 10:14:57 +0300

    Delete pwa_galimova_pi21-1v directory

--- pwa_galimova_pi21-1v/App.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) { // Проверка поддержки Service Worker
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js'); // Регистрация sw.js при загрузке страницы
-  });
-}
